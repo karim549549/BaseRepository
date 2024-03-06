c59919539b022269f92630926bc41cf8fb6e67bb
Reservation <113533406+karim549549@users.noreply.github.com>
2024-03-06 21:18:08 +0200

    BaseRepository.cs

--- BaseRepository.cs
+++ BaseRepository.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineCoachingSystem.Repository;
 using OnlineCoachingSystem.Repository.IRepositories;
-using System;
+
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,107 +11,91 @@ using System.Threading.Tasks;
 
 namespace OnlineCoachingSystem.EF.Implementation
 {
-    public class BaseRepository<Entity> : IBaseRepository<Entity> where Entity : class
+    public class BaseRepository<Entity>  where Entity : class
     {
         protected ApplicationDbContext _DbContext;
 
-        internal DbSet<Entity> MyDbSet;
+        internal Microsoft.EntityFrameworkCore.DbSet<Entity> MyDbSet;
         public BaseRepository(ApplicationDbContext DbContext) 
         { 
             _DbContext = DbContext;
             MyDbSet = _DbContext.Set<Entity>();
-        } 
-        
+        }
 
-        async Task<IEnumerable<Entity>> IBaseRepository<Entity>.AddRangeAsync(IEnumerable<Entity> entities)
+
+        public async Task<IEnumerable<Entity>> AsyncAddRange(IEnumerable<Entity> entities)
         {
             await MyDbSet.AddRangeAsync(entities);
             return entities;
         }
 
-          void IBaseRepository<Entity>.AttachRange(IEnumerable<Entity> entities)
+        public void RangeAttack(IEnumerable<Entity> entities)
         {
              MyDbSet.AttachRange(entities);   
         }
 
-        public async Task<int> IBaseRepository<Entity>.CountAsync(Expression<Func<Entity, bool>>? criteria)
+        public async Task<int>  AsyncCount(Expression<Func<Entity, bool>>? criteria)
         {
             return await MyDbSet.CountAsync(criteria);
         }
 
-        void IBaseRepository<Entity>.Delete(Entity entity)
+        public void DeleteConditionRangeAsync( Expression<Func<Entity, bool>>? criteria)
         {
-            throw new NotImplementedException();
+            IQueryable<Entity> query;
+            if (criteria != null)
+                query = MyDbSet.Where(criteria);
+            else
+                query = MyDbSet;
+            query.ExecuteDeleteAsync();
+        }
+        public  void DeleteRange(IEnumerable<Entity> entities)
+        {
+             MyDbSet.RemoveRange(entities);
         }
 
-        void IBaseRepository<Entity>.DeleteRange(IEnumerable<Entity> entities)
+        public async Task<IEnumerable<Entity>> FindAllAsync(Expression<Func<Entity, bool>>? criteria, int? take, int? skip,
+        Expression<Func<Entity , object>> orderBy = null, string orderByDirection = MagicStrings.Ascending)
         {
-            throw new NotImplementedException();
+            IQueryable<Entity> query;
+            if (criteria !=null)
+                     query = MyDbSet.Where(criteria);
+            else
+                     query = MyDbSet;
+
+            if (take.HasValue)
+                query = query.Take(take.Value);
+
+            if (skip.HasValue)
+                query = query.Skip(skip.Value);
+
+            if (orderBy != null)
+            {
+                if (orderByDirection == MagicStrings.Ascending)
+                    query = query.OrderBy(orderBy);
+                else
+                    query = query.OrderByDescending(orderBy);
+            }
+
+            return await query.ToListAsync();
         }
 
-        Entity IBaseRepository<Entity>.Find(Expression<Func<Entity, bool>> criteria, string[] includes)
+        public async Task<IEnumerable<Entity>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return  await MyDbSet.ToListAsync();
         }
 
-        IEnumerable<Entity> IBaseRepository<Entity>.FindAll(Expression<Func<Entity, bool>> criteria, string[] includes)
+        public async Task<Entity> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await MyDbSet.FindAsync(id);
         }
 
-        IEnumerable<Entity> IBaseRepository<Entity>.FindAll(Expression<Func<Entity, bool>> criteria, int take, int skip)
+        public  IEnumerable<Entity> AsyncUpdateRange(IEnumerable<Entity> entities)
         {
-            throw new NotImplementedException();
+            MyDbSet.UpdateRange(entities);
+            return  entities;
         }
 
-        //IEnumerable<Entity> IBaseRepository<Entity>.FindAll(Expression<Func<Entity, bool>> criteria, int? take, int? skip, Expression<Func<Entity, object>> orderBy, string orderByDirection)
-        //{
+        /***********************/
 
-        //}
-
-        Task<IEnumerable<Entity>> IBaseRepository<Entity>.FindAllAsync(Expression<Func<Entity, bool>> criteria, string[] includes)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<IEnumerable<Entity>> IBaseRepository<Entity>.FindAllAsync(Expression<Func<Entity, bool>> criteria, int skip, int take)
-        {
-            throw new NotImplementedException();
-        }
-
-        //Task<IEnumerable<Entity>> IBaseRepository<Entity>.FindAllAsync(Expression<Func<Entity, bool>> criteria, int? skip, int? take, Expression<Func<Entity, object>> orderBy, string orderByDirection)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        Task<Entity> IBaseRepository<Entity>.FindAsync(Expression<Func<Entity, bool>> criteria, string[] includes)
-        {
-            throw new NotImplementedException();
-        }
-
-        IEnumerable<Entity> IBaseRepository<Entity>.GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<IEnumerable<Entity>> IBaseRepository<Entity>.GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        Entity IBaseRepository<Entity>.GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Task<Entity> IBaseRepository<Entity>.GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        Entity IBaseRepository<Entity>.Update(Entity entity)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
